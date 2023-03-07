daa43d1601d57f1542c94706dfc63f366be8a991
Nevish-302 <shiven302@gmail.com>
2023-03-08 02:19:00 +0530

    tiny-c

--- Backend/app.js
+++ Backend/app.js
@@ -13,8 +13,8 @@ app = express()
 
 app.use(bodyParser.urlencoded({ extended: true })); 
 app.use(bodyParser.json());     
-app.use(cors({credentials: true, origin: '*'}))
-console.log('*')
+app.use(cors({credentials: true, origin: 'https://goresume-ten.vercel.app'}))
+console.log('https://goresume-ten.vercel.app')
 app.use(cookieParser())
 
 //Mongodb URI
