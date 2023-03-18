f9eb5f6321e6820d8d651419d4d8a3254234f383
Nevish-302 <shiven302@gmail.com>
2023-03-19 00:15:45 +0530

    cors-try

--- Backend/app.js
+++ Backend/app.js
@@ -13,7 +13,7 @@ app = express()
 
 app.use(bodyParser.urlencoded({ extended: true })); 
 app.use(bodyParser.json());     
-app.use(cors({credentials: true, origin: 'https://goresume-ten.vercel.app'}))
+app.use(cors({credentials: true, origin: 'https://goresume-ten.vercel.app', methods:'GET, POST, OPTIONS'}))
 app.use(cookieParser())
 
 //Mongodb URI
